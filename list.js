304795d2086c26b8a16da2f48be30800ffb7a72f
min <ken1336@naver.com>
2019-11-13 00:23:14 +0900

    Test deployment with Travis CI

---
+++ list.js
@@ -0,0 +1,22 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const params = {
+    TableName: process.env.tableName,
+    // 'KeyConditionExpression' defines the condition for the query
+    // - 'userId = :userId': only return items with matching 'userId'
+    //   partition key
+    // 'ExpressionAttributeValues' defines the value in the condition
+    // - ':userId': defines 'userId' to be Identity Pool identity id
+    //   of the authenticated user
+    KeyConditionExpression: "userId = :userId",
+    ExpressionAttributeValues: {
+      ":userId": event.requestContext.identity.cognitoIdentityId
+    }
+  };
+  const result = await dynamoDbLib.call("query", params)
+  .then(result => result.Items? success(result.Items) : failure({ status: false, error: "Item not found." }))
+  .catch(e => failure({ status: false }));
+  console.log(JSON.parse(result.body).map(v => v));
+}
