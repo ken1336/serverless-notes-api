3fd1202ec60da6856402e6cd9f2d2f5d50cde008
min <ken1336@naver.com>
2019-12-22 23:40:28 +0900

    Test adding Terraform script2

--- list.js
+++ list.js
@@ -15,6 +15,7 @@ export async function main(event, context) {
       ":userId": event.requestContext.identity.cognitoIdentityId
     }
   };
+  console.log("This is test for Terraform!");
   const result = await dynamoDbLib.call("query", params)
   .then(result => result.Items? success(result.Items) : failure({ status: false, error: "Item not found." }))
   .catch(e => failure({ status: false }));
