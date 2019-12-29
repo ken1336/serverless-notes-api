407a0856b5c20bd5f5cc408f56b8df0574191a96
min <ken1336@naver.com>
2019-12-30 01:18:54 +0900

    Github Action test with Terraform

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-exports.handler = (event, context, callback) => {
-    console.log('Hello, logs!');
-    callback(null, 'great success');
-}
+exports.handler = function(event, context, callback) {
+    var response = {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/html; charset=utf-8'
+      },
+      body: '<p>Hello world!</p>'
+    }
+    callback(null, response)
+  }
