b7e4931bb223bbc0614d91d35d9e7fbee831e340
min <ken1336@naver.com>
2019-12-31 22:50:01 +0900

    Adding Congito Terraform script

---
+++ Terraform/Cognito.tf
@@ -0,0 +1,25 @@
+resource "aws_cognito_user_pool" "pool" {
+  name = "mypool"
+  auto_verified_attributes = ["email"]
+}
+resource "aws_cognito_identity_provider" "pool_provider" {
+  user_pool_id  = "${aws_cognito_user_pool.pool.id}"
+  provider_name = "Google"
+  provider_type = "Google"
+
+  provider_details = {
+    authorize_scopes = "email"
+    client_id        = "your client_id"
+    client_secret    = "your client_secret"
+  }
+
+  attribute_mapping = {
+    email    = "email"
+    username = "sub"
+  }
+}
+resource "aws_cognito_user_pool_client" "client" {
+  name = "client"
+
+  user_pool_id = "${aws_cognito_user_pool.pool.id}"
+}
