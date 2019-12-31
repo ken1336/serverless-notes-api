deb95a5d344966dcc1b8e2f9d783de94fb04271c
min <ken1336@naver.com>
2020-01-01 00:32:34 +0900

    Adding sample webhosting script to Terraform7

--- Terraform/S3StaticHosting.tf
+++ Terraform/S3StaticHosting.tf
@@ -11,28 +11,28 @@ resource "aws_s3_bucket_policy" "site" {
 
 data "aws_iam_policy_document" "site_public_access" {
   statement {
-    actions = ["s3:GetObject"]
+    actions   = ["s3:GetObject"]
     resources = ["${aws_s3_bucket.static_hosting.arn}/*"]
 
     principals {
-      type = "AWS"
+      type        = "AWS"
       identifiers = ["*"]
     }
   }
 
   statement {
-    actions = ["s3:ListBucket"]
+    actions   = ["s3:ListBucket"]
     resources = ["${aws_s3_bucket.static_hosting.arn}"]
 
     principals {
-      type = "AWS"
+      type        = "AWS"
       identifiers = ["*"]
     }
   }
 }
 resource "aws_s3_bucket_object" "object" {
-  bucket = "${aws_s3_bucket.static_hosting.id}"
-  key    = "index.html"
-  source = "../index.html"
+  bucket       = "${aws_s3_bucket.static_hosting.id}"
+  key          = "index.html"
+  source       = "../index.html"
   content_type = "text/html"
 }
