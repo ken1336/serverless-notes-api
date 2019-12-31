1c2d4f296e12e24e30a9837a3564decdbcb18808
min <ken1336@naver.com>
2020-01-01 00:19:03 +0900

    Adding sample webhosting script to Terraform2

--- Terraform/S3StaticHosting.tf
+++ Terraform/S3StaticHosting.tf
@@ -35,7 +35,8 @@ resource "aws_s3_bucket_object" "object" {
   key    = "index.html"
   source = "../index.html"
   content_type = "text/html"
-  etag = "${md5(file("../index.html"))}"
+  # etag = "${md5(file("../index.html"))}"
+  etag = "${filemd5("../index.html")}"
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
